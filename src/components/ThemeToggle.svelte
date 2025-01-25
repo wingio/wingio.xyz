a15a36af675faccdd9b0c2d1af7447270a69de58
Wing <44992537+wingio@users.noreply.github.com>
2025-01-24 22:52:25 -0500

    Add footer with theme toggle

---
+++ src/components/ThemeToggle.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import { getTheme, setTheme } from "@wing/scripts/theme"
+
+    let toggled = $state.raw(getTheme() == "dark");
+
+    function onClick(e: MouseEvent) {
+        setTheme();
+        toggled = getTheme() == "dark"
+    }
+</script>
+
+<button on:click={onClick}>
+    <div>
+        {#if toggled}
+            <slot name="toggled" />
+        {:else}
+            <slot name="untoggled" />
+        {/if}
+    </div>
+</button>
+
+<style lang="scss">
+    button {
+        all: unset;
+        cursor: pointer;
+        border-radius: 50%;
+        transition: all 0.5s;
+
+        div {
+            width: 40px;
+            height: 40px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        &:hover {
+            background: color-mix(in srgb, var(--onSurfaceAlt) 8%, transparent);
+        }
+    }
+</style>
--- src/scripts/theme.ts
+++ src/scripts/theme.ts
@@ -1,13 +1,15 @@
 import "./polyfills";
 
 export function setTheme(dark = !document.body.classList.contains("dark")) {
+    const oldTheme = getTheme();
     const newTheme = dark ? "dark" : "light";
     localStorage.setItem("theme", newTheme);
 
     // temporarily disable transitions to avoid color transitions
     document.body.classList.add("disable-transitions");
 
-    document.body.classList.toggle("dark", dark);
+    document.body.classList.add(newTheme);
+    document.body.classList.remove(oldTheme);
 
     setImmediate(() => document.body.classList.remove("disable-transitions"));
 
